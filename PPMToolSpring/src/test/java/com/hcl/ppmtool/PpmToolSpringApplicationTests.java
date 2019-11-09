e162b16e6f43f90c1a3a60e697f9b009d006f30c
Ravi Kumar Gatram <ravikumar.g@hcl.com>
2019-11-09 18:13:49 +0530

    first commit

---
+++ PPMToolSpring/src/test/java/com/hcl/ppmtool/PpmToolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
