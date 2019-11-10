f84f9d3f5f948ecc93c532512c4241987458b5ba
Ravi Kumar Gatram <ravikumar.g@hcl.com>
2019-11-10 20:44:47 +0530

    Create Project from React

--- ppmtool-react-client/src/component/Project/AddProject.js
+++ ppmtool-react-client/src/component/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -29,7 +32,7 @@ class AddProject extends Component {
       endDate: this.state.endDate
     };
 
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -107,4 +110,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
